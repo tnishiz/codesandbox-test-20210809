ebc64663a73c6e6fe2e88980b5b1e54b336c28f3
tnishiz <85749872+tnishiz@users.noreply.github.com>
2021-08-09 13:19:25 +0900

    演習24

--- src/index.js
+++ src/index.js
@@ -94,3 +94,46 @@
 // const [ name, age] = myProfile;
 // const message4 =`名前は${name}です。年齢は${age}です｡`;
 // console.log(massage4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("にしやん");
+
+/**
+ * スプレッド構文...
+ */
+
+//配列の展開
+//  const arr1 = [ 1, 5];
+// //  console.log(arr1);
+// //  console.log(...arr1);
+
+//  const sumFunc = (num1, num2) => console.log(num1 + num2);
+// //  sumFunc(arr1[0], arr1[1]);
+
+//  sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const[num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 =[...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
